f7ab895228274238126caeb49b9086321fc42ccf
Grzegorz Cichosz <g.cichosz@students.mimuw.edu.pl>
2021-04-29 15:14:07 +0200

    added first prototypes of Poly and Mono parsing functions

---
+++ src/parse.c
@@ -0,0 +1,99 @@
+
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+#include <assert.h>
+
+#include "poly.h"
+#include "poly_lib.h"
+#include "parse.h"
+
+
+/*
+ * <poly> ::= <mono-list> | <coeff>
+ * <mono-list> ::= <mono> { + <mono-list> }
+ * <mono> ::= ( <poly>, <exp> )
+ *
+ * <coeff> ::= -9223372036854775808..9223372036854775807
+ * <exp> ::= 0..2147483647
+ */
+
+/* static long ReadNumber(const char* s) {
+ *   char* err;
+ *   long res = strtol(src, &strto_err, 10);
+ * } */
+
+void RangeError(void);
+
+/* to na razie czyta jedynie listę, jak odróżnić koef? */
+bool ParsePoly(char* src, char** err, Poly* p)
+{
+
+  char* strto_err;
+  poly_coeff_t c;
+  bool mono_read;
+  Mono m;
+
+  if (isdigit(*src)) {
+    c = strtol(src, &strto_err, 10);
+
+    if (errno == ERANGE) {
+      RangeError();
+      return false;
+    }
+
+    if (*strto_err != '\0' && *strto_err != ',')
+      return false;
+
+    *p = PolyFromCoeff(c);
+    return true;
+  }
+
+  *p = PolyZero();
+
+  while (*src != '\n' && *src != ',' && *src != '\0') {
+    if (*src == '+' || isspace(*src) || *src != '(') {
+      ++*src;
+    }
+
+    mono_read = ParseMono(src, err, &m);
+
+    if (!mono_read) {
+      PolyDestroy(p);
+      MonoDestroy(&m);
+      return false;
+    }
+
+    MonoListInsert(&p->list, &m);
+    src = *err;
+  }
+
+  return true;
+}
+
+bool ParseMono(char* src, char** err, Mono* m)
+{
+  bool poly_read;
+  Poly p;
+  long e;
+  char* strto_err;
+  assert(*src == '(');
+
+  poly_read = ParsePoly(src + 1, err, &p);
+
+  if (!(**err == ',' && poly_read)) {
+    /* błond */
+    return false;
+  }
+
+  src = *err + 1;
+  e = strtol(src, &strto_err, 10);
+  if (errno == ERANGE || e > 2147483647 || e < 0) {
+    RangeError();
+    PolyDestroy(&p);
+    return false;
+  }
+
+  *m = MonoFromPoly(&p, e);
+  return true;
+}
---
+++ src/parse.h
@@ -0,0 +1,60 @@
+#ifndef _PARSE_H_
+#define _PARSE_H_
+
+
+#include "poly.h"
+
+struct Command {
+  enum {ADD, AT, DEG_BY} command;
+  union {
+    poly_coeff_t at_arg;
+    unsigned long deg_arg; 
+  };
+};
+
+struct input {
+  enum {COMMAND, POLY, ERROR} kind;
+  union {
+    Poly poly;
+    struct Command cmnd;    
+  };
+  size_t linum;  
+};
+
+/*
+ * <poly> ::= <mono> { + <poly> } | <coeff>
+ * <mono> ::= ( <poly>, <exp> )
+ * 
+ * <coeff> ::= -9223372036854775808..9223372036854775807
+ * <exp> ::= 0..2147483647
+ */
+
+
+/* err jak w strtodzie */
+/**
+ * Próba sparsowania __wielomianu__ ze stringa @p src. Ustawia @p err na pierwszy
+ * niewczytany znak (coś jak funkcja biblioteczna `strtod`) i pod @p p ładuje
+ * wczytany wielomian.
+ * @param[in] src : źródło do wczytania wielomianu zeń
+ * @param[in,out] err : err, gdzie skończył wczytywanie
+ * @param[out] p : miejsce na wynikowy wielomian
+ * @return czy udało się wczytać wielomian
+ */
+bool ParsePoly(char* src, char** err, Poly* p);
+
+/**
+ * Analogicznie do ParsePoly -- wczytuje z napisu @p src __jednomian__, który
+ * zapisuje w @p m. Pod @p err niewczytana część stringa.
+ * @param[in] src : źródło do wczytania jednomianu zeń
+ * @param[in,out] err : err, gdzie skończył wczytywanie
+ * @param[out] p : miejsce na wynikowy jednomian
+ * @return czy udało się wczytać jednomian
+ */
+bool ParseMono(char* src, char** err, Mono* m);
+
+/* idk. myślałem by to zrobić jakoś przy użyciu PolyAddMonos tj, że dostaje
+ * tablicę i na nią ładuje kolejne i następnie je dodaje, ale chyba prościej
+ * robić kolejne MonoListInserty */
+void ParsePolyMonos(char* src, char* err, void* thunk, size_t size);
+
+#endif
